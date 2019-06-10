2c862341286d730eecc16a94e313e31757795d11
Rodrigo Navarro Perez <rnavarroperez@sdsu.edu>
2019-06-09 18:52:25 -0700

    Finished av18 modules
    Subroutines come from a previous git repo on gitlab.com
    
    Signed-off-by: Rodrigo Navarro Perez <rnavarroperez@sdsu.edu>

--- src/av18.f90
+++ src/av18.f90
@@ -3,9 +3,12 @@ use types, only: dp
 use constants, only: hbar_c_MeV_fm, pion_c_mass_MeV, pion_0_mass_MeV, pion_mass_MeV
 implicit none
 private
+public av18_oper_basis
 
 real(dp), parameter :: hc=hbar_c_MeV_fm, mpic = pion_c_mass_MeV, mpi0=pion_0_mass_MeV, mpi=pion_mass_MeV
-real(dp), parameter, dimension(1:44) :: av18_parameters=[&
+integer, parameter :: n_parameters = 44
+real(dp), parameter :: f2=0.075_dp !< pion nucleon coupling constant
+real(dp), parameter, dimension(1:n_parameters) :: av18_parameters=[&
     &     -7.62701, &
     &   1815.49200, &
     &   1847.80590, &
@@ -80,6 +83,129 @@ subroutine av18_oper_basis(r,v_operator)
         stop
     endif
 
+    v_operator = 0._dp
+    x = mu*r
+     x0 = mu0*r
+     xc = muc*r
+    if (r.le.small) then
+        tpi=3*cpi**2*r/mu**3
+        ypi0=(mpi0/mpic)**2*(mpi0/3)*cpi*r/mu0
+        tpi0=3*cpi*ypi0/mu0**2
+        ypic=(mpic/3)*cpi*r/muc
+        tpic=3*cpi*ypic/muc**2
+    else
+        rcut=1-exp(-cpi*r*r) 
+        ypi=exp(-x)*rcut/x   
+        tpi=(1+(3+3/x)/x)*ypi*rcut 
+        ypi0=(mpi0/mpic)**2*(mpi0/3)*exp(-x0)*rcut/x0
+        tpi0=(1+(3+3/x0)/x0)*ypi0*rcut
+        ypic=(mpic/3)*exp(-xc)*rcut/xc
+        tpic=(1+(3+3/xc)/xc)*ypic*rcut
+    end if
+    ypi0=f2*ypi0
+    ypic=f2*ypic
+    tpi0=f2*tpi0
+    tpic=f2*tpic
+    tpi2=tpi*tpi 
+    ws=1/(1+exp((r-rws)*aiws)) 
+    ws0=1/(1+exp(-rws*aiws))   
+    wsp=ws*(1+aiws*exp(-rws*aiws)*ws0*r) 
+    wsx=ws*x
+    wsx2=wsx*x
+    dypi00=(mpi0/mpic)**2*(mpi0/3)*cpi/mu0 
+    dypic0=(mpic/3)*cpi/muc
+    ypi0p=ypi0-f2*dypi00*ws*r/ws0
+    ypicp=ypic-f2*dypic0*ws*r/ws0
+    ypi=(ypi0+2*ypic)/3
+    tpi=(tpi0+2*tpic)/3
+
+    p11pp=  av18_parameters( 1)*tpi2 + av18_parameters( 2)*wsp + av18_parameters( 3)*wsx2 + ypi0p
+    p11np=  av18_parameters( 1)*tpi2 + av18_parameters( 4)*wsp + av18_parameters( 3)*wsx2 - ypi0p+2*ypicp
+    p11nn=  av18_parameters( 1)*tpi2 + av18_parameters( 5)*wsp + av18_parameters( 3)*wsx2 + ypi0p
+    pt1pp=  av18_parameters( 6)*tpi2 + av18_parameters( 7)*wsx + av18_parameters( 8)*wsx2 + tpi0
+    pt1np=  av18_parameters( 6)*tpi2 + av18_parameters( 7)*wsx + av18_parameters( 8)*wsx2 - tpi0+2*tpic
+    pt1nn=  av18_parameters( 6)*tpi2 + av18_parameters( 7)*wsx + av18_parameters( 8)*wsx2 + tpi0
+    pls1=   av18_parameters( 9)*tpi2 + av18_parameters(10)*wsp + av18_parameters(11)*wsx2
+    pl211=  av18_parameters(12)*tpi2 + av18_parameters(13)*wsp + av18_parameters(14)*wsx2
+    pls21=  av18_parameters(15)*tpi2 + av18_parameters(16)*wsp + av18_parameters(17)*wsx2
+    p10=    av18_parameters(18)*tpi2 + av18_parameters(19)*wsp + av18_parameters(20)*wsx2 - ypi0p-2*ypicp
+    pt0=    av18_parameters(21)*tpi2 + av18_parameters(22)*wsx + av18_parameters(23)*wsx2 - tpi0-2*tpic
+    pls0=   av18_parameters(24)*tpi2 + av18_parameters(25)*wsp + av18_parameters(26)*wsx2
+    pl210=  av18_parameters(27)*tpi2 + av18_parameters(28)*wsp + av18_parameters(29)*wsx2
+    pls20=  av18_parameters(30)*tpi2 + av18_parameters(31)*wsp + av18_parameters(32)*wsx2
+    p01pp=  av18_parameters(33)*tpi2 + av18_parameters(34)*wsp - 3*ypi0p
+    p01np=  av18_parameters(35)*tpi2 + av18_parameters(36)*wsp - 3*(-ypi0p+2*ypicp)
+    p01nn=  av18_parameters(37)*tpi2 + av18_parameters(38)*wsp - 3*ypi0p
+    pl201=  av18_parameters(39)*tpi2 + av18_parameters(40)*wsp
+    p00=    av18_parameters(41)*tpi2 + av18_parameters(42)*wsp - 3*(-ypi0p-2*ypicp)
+    pl200=  av18_parameters(43)*tpi2 + av18_parameters(44)*wsp
+
+    ! p11pp=  -7.62701*tpi2+1815.4920*wsp+1847.8059*wsx2+ypi0p
+    ! p11np=  -7.62701*tpi2+1813.5315*wsp+1847.8059*wsx2-ypi0p+2*ypicp
+    ! p11nn=  -7.62701*tpi2+1811.5710*wsp+1847.8059*wsx2+ypi0p
+    ! pt1pp=   1.07985*tpi2 -190.0949*wsx -811.2040*wsx2+tpi0
+    ! pt1np=   1.07985*tpi2 -190.0949*wsx -811.2040*wsx2-tpi0+2*tpic
+    ! pt1nn=   1.07985*tpi2 -190.0949*wsx -811.2040*wsx2+tpi0
+    ! pls1=    -.62697*tpi2 -570.5571*wsp +819.1222*wsx2
+    ! pl211=    .06709*tpi2 +342.0669*wsp -615.2339*wsx2
+    ! pls21=    .74129*tpi2   +9.3418*wsp -376.4384*wsx2
+    ! p10=    -8.62770*tpi2+2605.2682*wsp +441.9733*wsx2-ypi0p-2*ypicp
+    ! pt0=    1.485601*tpi2-1126.8359*wsx +370.1324*wsx2-tpi0-2*tpic
+    ! pls0=     .10180*tpi2  +86.0658*wsp -356.5175*wsx2
+    ! pl210=   -.13201*tpi2 +253.4350*wsp   -1.0076*wsx2
+    ! pls20=    .07357*tpi2 -217.5791*wsp  +18.3935*wsx2
+    ! p01pp= -11.27028*tpi2+3346.6874*wsp-3*ypi0p
+    ! p01np= -10.66788*tpi2+3126.5542*wsp-3*(-ypi0p+2*ypicp)
+    ! p01nn= -11.27028*tpi2+3342.7664*wsp-3*ypi0p
+    ! pl201=    .12472*tpi2  +16.7780*wsp
+    ! p00=    -2.09971*tpi2+1204.4301*wsp-3*(-ypi0p-2*ypicp)
+    ! pl200=   -.31452*tpi2 +217.4559*wsp
+
+    p11=(p11pp+p11nn+p11np)/3
+    p11cd=(.5_dp*(p11pp+p11nn)-p11np)/6
+    p11cs=(p11pp-p11nn)/4
+    pt1=(pt1pp+pt1nn+pt1np)/3
+    pt1cd=(.5_dp*(pt1pp+pt1nn)-pt1np)/6
+    pt1cs=(pt1pp-pt1nn)/4
+    p01=(p01pp+p01nn+p01np)/3
+    p01cd=(.5_dp*(p01pp+p01nn)-p01np)/6
+    p01cs=(p01pp-p01nn)/4
+
+    v_operator(1)=.0625_dp*(9*p11+3*p10+3*p01+p00)
+    v_operator(2)=.0625_dp*(3*p11-3*p10  +p01-p00)
+    v_operator(3)=.0625_dp*(3*p11  +p10-3*p01-p00)
+    v_operator(4)=.0625_dp*(  p11  -p10  -p01+p00)
+    v_operator(5)=.25_dp*(3*pt1+pt0)
+    v_operator(6)=.25_dp*(  pt1-pt0)
+    v_operator(7)=.25_dp*(3*pls1+pls0)
+    v_operator(8)=.25_dp*(  pls1-pls0)
+    v_operator(9)= .0625_dp*(9*pl211+3*pl210+3*pl201+pl200)
+    v_operator(10)=.0625_dp*(3*pl211-3*pl210+  pl201-pl200)
+    v_operator(11)=.0625_dp*(3*pl211+  pl210-3*pl201-pl200)
+    v_operator(12)=.0625_dp*(  pl211-  pl210-  pl201+pl200)
+    v_operator(13)=.25_dp*(3*pls21+pls20)
+    v_operator(14)=.25_dp*(  pls21-pls20)
+    v_operator(15)=.25_dp*(3*p11cd+p01cd)
+    v_operator(16)=.25_dp*(  p11cd-p01cd)
+    v_operator(17)=pt1cd
+    v_operator(18)=p01cs
 end subroutine av18_oper_basis
-    
+
+subroutine av18_spin_isospin_basis(tz1,tz2,s,t,v_oper,v_c,v_t,v_ls,v_l2,v_ls2)
+    implicit none
+    integer, intent(in) :: tz1,tz2,s,t
+    real(dp), intent(in) :: v_oper(:)
+    real(dp), intent(out) :: v_c,v_t,v_ls,v_l2,v_ls2
+    integer :: s1ds2, t1dt2,t12
+    s1ds2=4*s-3
+    t1dt2=4*t-3
+    t12=3*tz1*tz2-t1dt2
+    v_c=v_oper(1)+t1dt2*v_oper(2)+s1ds2*v_oper(3)+s1ds2*t1dt2*v_oper(4)&
+        &+t12*v_oper(15)+s1ds2*t12*v_oper(16)+(tz1+tz2)*v_oper(18)
+    v_t=v_oper(5)+t1dt2*v_oper(6)+t12*v_oper(17)
+    v_ls=v_oper(7)+t1dt2*v_oper(8)
+    v_l2=v_oper(9)+t1dt2*v_oper(10)+s1ds2*v_oper(11)+s1ds2*t1dt2*v_oper(12)
+    v_ls2=v_oper(13)+t1dt2*v_oper(14)
+end subroutine av18_spin_isospin_basis
+
 end module av18

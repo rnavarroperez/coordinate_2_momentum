aa53f5e54b401f13abf35f81229490434aaa6d5a
Rodrigo Navarro Perez <rnavarroperez@sdsu.edu>
2019-06-11 17:13:06 -0700

    OPE is calculated in operator basis
    Signed-off-by: "Rodrigo Navarro Perez" <"rnavarroperez@sdsu.edu">

--- src/pion_exchange.f90
+++ src/pion_exchange.f90
@@ -14,6 +14,55 @@ subroutine v_one_pion_exch(r,v_oper_basis)
     real(dp) :: vc_11pp, vc_11np, vc_11nn, vc_01pp, vc_01np, vc_01nn
     real(dp) :: vt_11pp, vt_11np, vt_11nn, vt_01pp, vt_01np, vt_01nn
     real(dp) :: vc_10np, vc_00np, vt_10np, vt_00np
+
+    real(dp) :: vc_11_ci, vc_11_cd, vc_11_ca
+    real(dp) :: vc_01_ci, vc_01_cd, vc_01_ca
+    real(dp) :: vc_10_ci
+    real(dp) :: vc_00_ci
+
+    real(dp) :: vt_11_ci, vt_11_cd
+    real(dp) :: vt_10_ci
+
+    v_oper_basis = 0._dp
+
+    call v_ope_st(r,1,1, 1, 1,vc_11pp,vt_11pp)
+    call v_ope_st(r,1,1,-1, 1,vc_11np,vt_11np)
+    call v_ope_st(r,1,1,-1,-1,vc_11nn,vt_11nn)
+
+    call v_ope_st(r,0,1, 1, 1,vc_01pp,vt_01pp)
+    call v_ope_st(r,0,1,-1, 1,vc_01np,vt_01np)
+    call v_ope_st(r,0,1,-1,-1,vc_01nn,vt_01nn)
+
+    call v_ope_st(r,1,0,-1, 1,vc_10np,vt_10np)
+    call v_ope_st(r,0,0,-1, 1,vc_00np,vt_00np)
+
+    vc_11_ci = (vc_11pp + vc_11np + vc_11nn)/3
+    vc_01_ci = (vc_01pp + vc_01np + vc_01nn)/3
+    vc_10_ci = vc_10np
+    vc_00_ci = vc_00np
+
+    vt_11_ci = (vt_11pp + vt_11np + vt_11nn)/3
+    vt_10_ci = vt_10np
+    
+    vc_11_cd = ((vc_11pp + vc_11nn)/2 - vc_11np)/6
+    vc_01_cd = ((vc_01pp + vc_01nn)/2 - vc_01np)/6
+
+    vt_11_cd = ((vt_11pp + vt_11nn)/2 - vt_11np)/6
+
+    vc_11_ca = (vc_11pp - vc_11nn)/4
+    vc_01_ca = (vc_01pp - vc_01nn)/4
+
+    v_oper_basis( 1) = (9*vc_11_ci + 3*vc_10_ci + 3*vc_01_ci + vc_00_ci)/16
+    v_oper_basis( 2) = (3*vc_11_ci - 3*vc_10_ci +   vc_01_ci - vc_00_ci)/16
+    v_oper_basis( 3) = (3*vc_11_ci +   vc_10_ci - 3*vc_01_ci - vc_00_ci)/16
+    v_oper_basis( 4) = (  vc_11_ci -   vc_10_ci -   vc_01_ci + vc_00_ci)/16
+    v_oper_basis( 5) = (3*vt_11_ci + vt_10_ci)/4
+    v_oper_basis( 6) = (  vt_11_ci - vt_10_ci)/4
+    v_oper_basis(15) = (3*vc_11_cd + vc_01_cd)/4
+    v_oper_basis(16) = (  vc_11_cd - vc_01_cd)/4
+    v_oper_basis(17) = vt_11_cd
+    v_oper_basis(18) = (3*vc_11_ca + vc_01_ca)/4
+
 end subroutine v_one_pion_exch
 
 subroutine v_ope_st(r,s,t,tz1,tz2,vc,vt)
